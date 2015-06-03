d64a2b9555bc079e2bb40eda5a9c3fa70ab79fb7
Ryan Keiper <ryankeiper@gmail.com>
2015-06-03 11:44:52 -0400

    starting to add lasers

--- public/js/main.js
+++ public/js/main.js
@@ -9,31 +9,76 @@ var game = function(){
 	}
 
 	const GAME_STATE_TITLE = 0;
-	const GAME_STATE_NEW_LEVEL = 1;
-	const GAME_STATE_GAME_OVER = 2;
+	const GAME_STATE_NEW_GAME = 1;
+	const GAME_STATE_NEW_LEVEL = 2;
+	const GAME_STATE_PLAYER_START = 3;
+	const GAME_STATE_PLAY_LEVEL = 4;
+	const GAME_STATE_PLAYER_DIE = 5;
+	const GAME_STATE_GAME_OVER = 6;
 
-	var player = {};
-	player.width = 20;
-	player.height = 20;
-	player.halfW = 10;
-	player.halfH = 10;
-	player.rotationalVelocity = 7;
-	player.acceleration = .2;
-	player.maxSpeed = 8;
-	player.thrust = 0;
-	player.laserDelay = 5;
-	
-	var rotation = 0;
-	var x = window.innerWidth/2;
-	var y = window.innerHeight/2;
+	var currentGameState = 0;
+	var currentGameStateFunction = null;
 
-	var facingX = 0;
-	var facingY = 0;
-	var movingX = 0;
-	var movingY = 0;
+	function Player() {
+		this.width = 20;
+		this.height = 20;
+		this.halfW = 10;
+		this.halfH = 10;
+		this.rotationalVelocity = 7;
+		this.acceleration = .2;
+		// this.maxSpeed = 8;
+		this.thrust = 0;
+		this.laserDelay = 5;
+		this.sinceLastLaser = 0;
+		this.lasers = [];
+		this.x = window.innerWidth/2;
+		this.y = window.innerHeight/2;
+		this.rotation = 0;
+		this.facingX = 0;
+		this.facingY = 0;
+		this.movingX = 0;
+		this.movingY = 0;
+	};
+
+	function NewLaser(){
+		this.dx = 5*Math.cos(Math.PI*(player1.rotation)/180);
+		this.dy = 5*Math.sin(Math.PI*(player1.rotation)/180);
+		this.x = player1.x+player1.halfW;
+		this.y = player1.y+player1.halfH;
+		this.life = 60;
+		this.lifeCtr = 0;
+		this.width = 2;
+		this.height = 2;
+	}
+
+	var player1 = new Player();
 	
 	var keyPressList = [];
 
+	var opening = false;
+	var gameOver = false;
+
+	var score = 0;
+	var level = 0;
+	var playerShips = 3;
+
+	var xMin = 0;
+	var xMax = window.innerWidth;
+	var yMin = 0;
+	var yMax = window.innerHeight;
+
+	var scoreBigRock = 100;
+	var scoreMedRock = 150;
+	var scoreSmallRock = 200;
+	var maxRockSpeedByLevel = 1;
+
+	const BIG_ROCK = 1;
+	const MED_ROCK = 2;
+	const SMALL_ROCK = 3;
+
+	var players = [];
+	var rocks = [];
+	var particles = [];
 
 	var render = function(){
 		ctx.canvas.width  = window.innerWidth;
@@ -43,43 +88,59 @@ var game = function(){
 	var gameFrame = function(){
 
 		if(keyPressList[38] == true){
-			var radians = (rotation - 90) * Math.PI/180;
+			var radians = (player1.rotation - 90) * Math.PI/180;
 			facingX = Math.cos(radians);
 			facingY = Math.sin(radians);
 
-			var movingXNew = movingX + player.acceleration * facingX;
-			var movingYNew = movingY + player.acceleration * facingY;
+			// var movingXNew = player1.movingX + player1.acceleration * facingX;
+			// var movingYNew = player1.movingY + player1.acceleration * facingY;
 
-			var currentSpeed = Math.sqrt((movingXNew*movingXNew) + (movingYNew*movingYNew));
+			// var currentSpeed = Math.sqrt((movingXNew*movingXNew) + (movingYNew*movingYNew));
 
-			if(currentSpeed < player.maxSpeed){
-				movingX += player.acceleration * facingX;
-				movingY += player.acceleration * facingY;
-			}
+			// if(currentSpeed < player1.maxSpeed){
+			player1.movingX += player1.acceleration * facingX;
+			player1.movingY += player1.acceleration * facingY;
+			// }
 
-			player.thrust = 1;
+			player1.thrust = 1;
 		} else {
-			player.thrust = 0;
+			player1.thrust = 0;
 		}
 
 		if(keyPressList[37] == true){
-			rotation -= player.rotationalVelocity;
+			player1.rotation -= player1.rotationalVelocity;
 		}
 
 		if(keyPressList[39] == true){
-			rotation += player.rotationalVelocity;
+			player1.rotation += player1.rotationalVelocity;
 		}
 
-		x += movingX;
-		y += movingY;
+		if(keyPressList[32] == true){
+
+		}
+
+		player1.x += player1.movingX;
+		player1.y += player1.movingY;
+
+		if(player1.x > xMax){
+			player1.x = -player1.width;
+		} else if(player1.x < -player1.width) {
+			player1.x = xMax;
+		}
+
+		if(player1.y > yMax){
+			player1.y = -player1.height;
+		} else if(player1.y < -player1.height) {
+			player1.y = yMax;
+		}
 		
 		render();
 
-		var radians = rotation * Math.PI/180;
+		var radians = player1.rotation * Math.PI/180;
 		ctx.save();
 		ctx.setTransform(1,0,0,1,0,0);
 
-		ctx.translate(x+player.halfW,y+player.halfH);
+		ctx.translate(player1.x+player1.halfW,player1.y+player1.halfH);
 		ctx.rotate(radians);
 
 		ship();
@@ -106,7 +167,7 @@ var game = function(){
 	}
 	var thrusters = function(){
 	    // Exhaust
-	    if(player.thrust == 1){
+	    if(player1.thrust == 1){
 		    var my_gradient=ctx.createLinearGradient(-2,3,-2,18);
 			my_gradient.addColorStop(0,"black");
 			my_gradient.addColorStop(0.2,"red");
